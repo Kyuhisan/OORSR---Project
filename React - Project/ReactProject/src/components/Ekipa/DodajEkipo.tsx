ff0783d1ec799ef8335f6f26a6afc79cc0488307
Kyuhi <64851536+Kyuhisan@users.noreply.github.com>
2024-12-03 00:23:40 +0100

    Delete React - Project/ReactProject directory

--- React - Project/ReactProject/src/components/Ekipa/DodajEkipo.tsx
+++
@@ -1,5 +0,0 @@
-function DodajEkipo() {
-  return <></>;
-}
-
-export default DodajEkipo;
--- React - Project/ReactProject/src/components/Layout/Noga.tsx
+++
@@ -1,9 +0,0 @@
-function Noga() {
-  return (
-    <footer className="container text-center">
-      <p>@Matic Kuhar</p>
-    </footer>
-  );
-}
-
-export default Noga;
